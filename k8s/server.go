c17289faa0fc6977271e79014ae79789c6ba29d6
Allan Milani <allan.godev@gmail.com>
2022-11-15 10:07:28 -0300

    Checagens de Health da aplicação

--- k8s/server.go
+++ k8s/server.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 );
+
+var startedAt = time.Now();
 
 func main(){
 	http.HandleFunc("/", Hello);
 	http.HandleFunc("/configmap", ConfigMap);
 	http.HandleFunc("/secret", Secret);
+	http.HandleFunc("/healthz", Healthz);
 	http.ListenAndServe(":80", nil);
 }
 
@@ -35,4 +39,17 @@ func ConfigMap(w http.ResponseWriter, r *http.Request){
 		log.Fatalf("Error reading file: ", err);
 	}
 	fmt.Fprintf(w, "Hy Family: %s.", string(data));
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request){
+	duration := time.Since(startedAt);
+
+	// if duration.Seconds() > 25 { //Liveness
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500);
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200);
+		w.Write([]byte("ok"));
+	}
 }
